aada22ffe3af15391e4c8cbee6138b0eff86f67e
Daniel Ronkainen <daniel.ronkainen@wazabii.se>
2026-03-10 21:23:16 +0100

    refactor: rename props class and add small examples to router comment

--- routers/web.php
+++ routers/web.php
@@ -3,10 +3,22 @@
  * Web router
  *
  * @var RouterDispatcher $router
+ *
+ * FastRoute parameter patterns:
+ *
+ * {name}              Matches any segment except "/". (Example: /user/{name})
+ *
+ * {id:\d+}            Matches numeric values only. (Example: /post/{id})
+ *
+ * {name:[^/]+}        Explicit single path segment. (Example: /profile/{name:[^/]+})
+ *
+ * {slug:.+}           Matches everything including slashes. (Example: /cat/{slug:.+})
+ *
+ * {lang:(en|sv|de)}   Restricts parameter to specific values. (Example: /{lang}/docs)
  */
 
 use App\Controllers\StartController;
 use MaplePHP\Core\Router\RouterDispatcher;
 
 $router->get("/", [StartController::class, "index"]);
-$router->get("/{page:.+}", [StartController::class, "show"]);
+$router->get("/{page}", [StartController::class, "show"]);
